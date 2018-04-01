dcaec6ea45b17e66faa6cf978bd303ccf0cc7870
Shravan Dhar <shraonedhar@gmail.com>
2018-04-02 01:15:16 +0530

    admin | minor changes

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// starting page hide/show bars ********
 $("#Parent").click( () => {
     $(".A, .T, .SA").addClass("q");
     $(".P").removeClass("q");
@@ -17,3 +18,31 @@ $("#Teacher").click( () => {
     $(".P, .A, .SA").addClass("q");
     $(".T").removeClass("q");
 });
+
+// Admin Page hide-show bars *********
+$(".myBtn1").click( () => {
+    $(".acad-yr-plan, .rep-gen").addClass("q");
+    $(".view-edit").removeClass("q");
+});
+$(".myBtn2").click( () => {
+    $(".view-edit, .rep-gen").addClass("q");
+    $(".acad-yr-plan").removeClass("q");
+});
+$(".myBtn3").click( () => {
+    $(".view-edit, .acad-yr-plan").addClass("q");
+    $(".rep-gen").removeClass("q");
+});
+    // go to top
+window.onscroll = function () { scrollFunction() };
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
